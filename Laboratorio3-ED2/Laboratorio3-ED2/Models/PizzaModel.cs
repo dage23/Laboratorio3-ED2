db00511809779a13ceefb26074126aac859e88a2
Allan Davila <allan.davila@hotmail.com>
2019-11-09 21:10:26 -0600

    Completada SERIE 2 y 3

---
+++ Laboratorio3-ED2/Laboratorio3-ED2/Models/PizzaModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Laboratorio3_ED2.Models
+{
+    public class PizzaModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string NombrePizza { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string[] Ingredientes { get; set; }
+
+        public string Masa { get; set; }
+        public string Tamano { get; set; }
+        public int Porciones { get; set; }
+        public bool Queso { get; set; }
+    }
+}
